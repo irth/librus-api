eac03381adb64fe67c8d22bc4b28146e768f4d8e
Wojciech Kwolek <me@irth.pl>
2017-10-23 13:39:15 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import json
+from flask import Flask, request, abort
+from librus import librus
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/auth", methods=['POST'])
+def auth():
+    login = request.form['login']
+    password = request.form['password']
+
+    result = librus.Librus(login, password).login()
+    if result is None:
+        abort(401)
+    else:
+        return json.dumps({"DZIENNIKSID": result})
+
+
+class TimetableEncoder(json.JSONEncoder):
+    def default(self, o):
+        return o.__dict__ if isinstance(o, librus.Class) else o
+
+
+@app.route("/timetable", methods=['POST'])
+def timetable():
+    try:
+        login = request.form['login']
+        password = request.form['password']
+        l = librus.Librus(login, password)
+        if l.login() is None:
+            abort(401)
+
+        return json.dumps(l.get_timetable(), cls=TimetableEncoder)
+    except KeyError:
+        cookie = request.form['cookie']
+        return json.dumps(librus.Librus(cookie=cookie).get_timetable(), cls=TimetableEncoder)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
